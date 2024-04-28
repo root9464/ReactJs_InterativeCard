c147a28eaae379f29aa233a1463ec1fcda6d6ee0
iO <senpai9464@yandex.ru>
2024-04-28 22:27:12 +0500

    остались стили

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,13 @@
 
 import { LayoutGroup } from 'framer-motion';
 import { useState } from 'react';
-import frame1 from '../assets/img/frame1.png';
-import frame2 from '../assets/img/frame2.png';
-import frame3 from '../assets/img/frame3.png';
-import frame4 from '../assets/img/frame4.png';
 import { Card } from '../components/Card/Card';
-import { CardProps } from '../components/Card/type';
 import { Frames } from '../components/Frames/Frames';
-import style from './main.module.sass';
+import { data, frames } from './consts';
 
+import style from './main.module.sass';
 export const App = () => {
   const [cardNumber, setCardNumber] = useState<number>(1);
-  const frames = [frame1, frame2, frame3, frame4];
-  const data: CardProps[] = [
-    {
-      title: 'Для поиска сотрудников',
-      text: 'Следите за тенденциями рынка, чтобы вовремя корректировать стратегию подбора персонала. Оценивайте средние зарплаты в разных регионах и профобластях. Находите регионы-доноры, откуда можно привлекать сотрудников.',
-    },
-    {
-      title: 'Для поиска работы',
-      text: 'Изучайте уровень конкуренции в интересующих вас профобластях и регионах с помощью показателя hr.индекс, а также средний уровень зарплат и динамику вакансий.',
-    },
-    {
-      title: 'Для запуска новых продуктов',
-      text: 'Изучайте ситуацию на рынке труда, чтобы понимать возможности аудитории в разных городах и регионах. Оценивайте востребованность профессий, если вы планируете запуск новых образовательных продуктов.',
-    },
-    {
-      title: 'Для статей и отчётов о ситуации на рынке',
-      text: 'Собирайте нужные для ваших аналитических материалов данные самостоятельно и онлайн. Статистика поможет составить достоверную картину о ситуации в разных регионах и профессиональных сферах.'
-    }
-  ];
-
   const [isOpen, setIsOpen] = useState<boolean[]>(data.map((_, i) => i === 0));
 
   const toggleCardNumber = (i: number) => {
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -17,13 +17,17 @@ export const Card = ({ title, text, icon, isOpen, onClick }: CardProps): JSX.Ele
                     <img src={icon} alt="" />
                     <h3>{title}</h3>
                 </motion.div>
-                {
-                    isOpen? (
-                        <motion.div layout className={style.text}>
-                            <motion.p layout="preserve-aspect">{text}</motion.p>
-                        </motion.div>
-                    ) : null
-                }
+
+             
+                <motion.div layout className={style.text}>
+                    {
+                        isOpen ? (
+                            <motion.p layout="position" initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{duration: 0.5, delay: 0.1, ease: [0, 0.71, 0.2, 1.01]}}>{text}</motion.p>
+                        ): null
+                    }
+                </motion.div>
+
+
             </motion.div>
         </motion.div>
     )
